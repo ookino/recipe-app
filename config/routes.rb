ba1a1af7f68a88f75139d53abfcd3d504e61424a
Busiwa24 <busiwaliuma@gmail.com>
2022-03-15 15:43:44 +0200

    Add users devise in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'foods/index'
+  get 'foods/new'
+  get 'recipes/index'
+  get 'recipes/show'
+  get 'recipes/new'
+  get 'recipes/edit'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+​
+  resources :foods, only: %i[index new create destroy]
+  resources :recipes, only: %i[index show new create edit update destroy]
+​
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'foods#index'
 end
