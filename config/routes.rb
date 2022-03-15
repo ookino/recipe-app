0ef19e96e3539666dbbda0b2d9ed71051f8bfe00
Yaseer Anaisi Okino <37089367+yaseerokino@users.noreply.github.com>
2022-03-15 21:55:12 +0100

    Fix linter errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   resources :recipes
   resources :foods
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "foods#index"
+  root 'foods#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,26 +12,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_15_202719) do
+ActiveRecord::Schema[7.0].define(version: 20_220_315_202_719) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "foods", force: :cascade do |t|
-    t.string "name"
-    t.string "measurement_unit"
-    t.decimal "price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'foods', force: :cascade do |t|
+    t.string 'name'
+    t.string 'measurement_unit'
+    t.decimal 'price'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "recipes", force: :cascade do |t|
-    t.string "name"
-    t.integer "preperation_time"
-    t.integer "cooking_time"
-    t.text "description"
-    t.boolean "public"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'recipes', force: :cascade do |t|
+    t.string 'name'
+    t.integer 'preperation_time'
+    t.integer 'cooking_time'
+    t.text 'description'
+    t.boolean 'public'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
-
 end
