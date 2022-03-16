a801a221119874b653afddd6ea10451126c4d815
Yaseer Anaisi Okino <37089367+yaseerokino@users.noreply.github.com>
2022-03-16 10:57:10 +0100

    Update recipe controller and model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,6 @@ class User < ApplicationRecord
          :validatable,
          :confirmable
 
+  has_many :recipes, dependent: :destroy
   validates :name, presence: true, length: { minimum: 3 }
 end
