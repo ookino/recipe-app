7c5a01c1fd998c5c9851b3be4b90c23bf779478e
Busiwa24 <busiwaliuma@gmail.com>
2022-03-18 04:01:30 +0200

    Add recipe input functionalities

---
+++ app/controllers/recipe_foods-controller.rb
@@ -0,0 +1,53 @@
+class RecipesController < ApplicationController
+    load_and_authorize_resource
+    before_action :set_recipe, only: %i[show edit update destroy]
+  
+    def index
+      @recipes = current_user.recipes
+    end
+  
+    def show; end
+  
+    def new
+      @recipe = Recipe.new
+    end
+  
+    # GET /recipes/1/edit
+    def edit; end
+  
+    def create
+      @recipe = current_user.recipes.new(recipe_params)
+  
+      respond_to do |format|
+        if @recipe.save
+          format.html { redirect_to recipe_url(@recipe), notice: 'Recipe was successfully created.' }
+          format.json { render :show, status: :created, location: @recipe }
+        else
+          format.html { render :new, status: :unprocessable_entity }
+          format.json { render json: @recipe.errors, status: :unprocessable_entity }
+        end
+      end
+    end
+  
+    def destroy
+      @recipe.destroy
+  
+      respond_to do |format|
+        format.html { redirect_to recipes_url, notice: 'Recipe was successfully destroyed.' }
+        format.json { head :no_content }
+      end
+    end
+  
+    private
+  
+    # Use callbacks to share common setup or constraints between actions.
+    def set_recipe
+      @recipe = Recipe.find(params[:id])
+    end
+  
+    # Only allow a list of trusted parameters through.
+    def recipe_params
+      params.require(:recipe).permit(:name, :preparationTime, :cookingTime, :description, :public)
+    end
+  end
+  
