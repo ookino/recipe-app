39d9dfe068b8a5c77b8a10b0245e840d0dfbfe90
Busiwa24 <busiwaliuma@gmail.com>
2022-03-18 13:49:46 +0200

    Add shopping list info

---
+++ app/controllers/shopping_list_controller.rb
@@ -0,0 +1,16 @@
+class ShoppingListsController < ApplicationController
+    def sum(array)
+      sum = 0
+      array.each do |number|
+        sum += number.quantity * number.food.Price
+      end
+      sum
+    end
+  
+    def index
+      @recipe = Recipe.find(params[:recipe_id])
+      @recipe_foods = RecipeFood.all
+      @food = Food.all
+      @total_price = sum(@recipe.recipe_foods.includes([:food]))
+    end
+  end
